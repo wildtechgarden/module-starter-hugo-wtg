68a9341ab23b9099a786ad85d979153455a95a62
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-09-04 17:43:54 -0400

    Fix directories for deploy script and add modules
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/local_build_deploy.sh
+++ scripts/local_build_deploy.sh
@@ -8,6 +8,10 @@ rm -rf public
 
 export BASEURL="https://www.module-starter.wtg-demos.ca/"
 export HUGO_RESOURCEDIR="$(pwd)"/resources
+export SITECONFIG="$(pwd)"/tests/config/hugo.toml
+export TARGET="$(pwd)"/public
+export CURDIR="$(pwd)"
 
-cd tests/config && hugo --gc --minify -b $BASEURL --source "$(pwd)" --destination "$(pwd)"/public --config "$(pwd)"/tests/config/hugo.toml
+cd tests/config && hugo --gc --minify -b $BASEURL --source "$(pwd)" --destination "${TARGET}" --config "${SITECONFIG}"
+cd "${CURDIR}"
 rclone sync --progress public/ wtg-modstarter:./
